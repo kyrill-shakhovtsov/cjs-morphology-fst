9a7ce60e2a87cd9c892ff902cc723d1b7ec3420b
Kyrill Shakhovtsov <etvex@yandex.ru>
2021-10-20 00:33:48 +0300

    Улучшены analyse.sh и generate.sh
    Добавлена возможность выбора между анализатором и «гадалкой».

--- analyse.sh
+++ analyse.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
 
-echo $1 | flookup fst/cjs-clean-analyser.bin
+case "$1" in
+	'-a')
+		echo $2 | flookup fst/cjs-clean-analyser.bin
+		;;
+	'-g')
+		echo $2 | flookup fst/cjs-guesser.bin
+		;;
+	*)
+		echo -e "$0 {-a|-g} словоформа\n\t-a\tиспользует анализатор: $0 -a киреечилербе\n\t-g\tиспользует «гадалку»:  $0 -g кертиктеримде"
+esac
--- generate.sh
+++ generate.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
-echo $1 | flookup -i fst/cjs-clean-analyser.bin
+case "$1" in
+	'-g')
+		echo $2 | flookup -i fst/cjs-guesser.bin
+		;;
+	'-a')
+		echo $2 | flookup -i fst/cjs-clean-analyser.bin
+		;;
+	*)
+		echo -e "$0 {-a|-g} разбор\n\t-a\tиспользует анализатор: $0 -a ада[N]-PL-DAT\n\t-g\tиспользует «гадалку»:  $0 -g кергиш[N]:Guess-SG-DAT"
+esac
 
-# echo $1 | flookup -i -a fst/cjs-clean-analyser-guesser.bin
-
-# echo $1 | flookup -i fst/cjs-guesser.bin
